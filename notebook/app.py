8a66806185c268376ce014cd392a7b1a6f597d86
aditya <aditya.sharmahdr@gmail.com>
2024-05-15 15:56:58 +0530

    artifacts and data ingestion done

--- notebook/app.py
+++ notebook/app.py
@@ -5,7 +5,10 @@ if __name__=="__main__":
     logging.info("testing logging.py")
 
 try:
-    a=1/0
+    a=1
+    b="hehe"
+    c=a+b;
+    print(c)
     
 except Exception as e:
     logging.info("CUstom exception")
